ab35fb612d07ebed2f5f48f1315033221276b089
Neil Jay Warner <neilwarner@airstrip.com>
2016-01-03 16:24:27 -0600

    Iniital commit

---
+++ mobile/src/main/java/com/spiritflightapps/dfw2016verses/VerseActivity.java
@@ -0,0 +1,13 @@
+package com.spiritflightapps.dfw2016verses;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class VerseActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_verse);
+    }
+}
